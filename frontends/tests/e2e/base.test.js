65360f7829ee66b7e43b39ee5eaa1f1fc81f9b5d
Nikolas De Giorgis <nikolas.degiorgis@gmail.com>
2025-10-02 10:10:14 +0100

    tests: enable automatic test on iOS.

--- frontends/tests/e2e/base.test.js
+++ frontends/tests/e2e/base.test.js
@@ -3,21 +3,42 @@ import { expect } from "chai";
 
 // --- Test ---
 describe("BitBoxApp Base Test", function () {
-  this.timeout(180000);
+  this.timeout(180000); // 3 minutes timeout for emulator/simulator boot
 
   let driver;
-  before(async () => {
 
-    const opts =  {
+  before(async () => {
+    const isIos = process.env.PLATFORM === "iOS";
+
+    const opts = {
       path: '/',
       port: 4723,
-      capabilities: {
-        platformName: 'Android',
-        'appium:deviceName': 'Android Emulator',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': `./apk/app-debug.apk`,
-        'appium:noReset': true,
-      }
+      connectionRetryTimeout: 300000, // 5 min
+      connectionRetryCount: 3,
+      capabilities: isIos
+        ? {
+            platformName: 'iOS',
+            'appium:deviceName': 'iPhone 15',
+            'appium:platformVersion': '17.5',
+            'appium:automationName': 'XCUITest',
+            'appium:showXcodeLog': true,
+            'appium:app': process.env.IOS_APP_PATH,
+            'appium:useNewWDA': true,
+            'appium:usePrebuiltWDA': true,
+            'appium:noReset': true,
+            'appium:wdaStartupRetries': 3,
+            'appium:wdaLaunchTimeout': 120000,
+            'appium:wdaStartupRetryInterval': 15000,
+            'appium:webviewConnectTimeout': 250000, // ms
+            'appium:webviewConnectRetries': 30,
+          }
+        : {
+            platformName: 'Android',
+            'appium:deviceName': 'Android Emulator',
+            'appium:automationName': 'UiAutomator2',
+            'appium:app': './apk/app-debug.apk',
+            'appium:noReset': true,
+          },
     };
 
     driver = await remote(opts);
@@ -25,7 +46,9 @@ describe("BitBoxApp Base Test", function () {
     // Switch to WebView if present
     const contexts = await driver.getContexts();
     const webview = contexts.find((c) => c.startsWith("WEBVIEW_"));
-    if (webview) await driver.switchContext(webview);
+    if (webview) {
+      await driver.switchContext(webview);
+    }
   });
 
   after(async () => {
