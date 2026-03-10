43de594834d728af0644f377f331e1da9fa284c7
Nikolas De Giorgis <nikolas.degiorgis@bitbox.swiss>
2026-03-10 11:26:30 +0000

    frontend: enable inactive swap destination accounts
    This allows user to select accounts to swap to even if those accounts
    are not active yet, effectively reducing friction for the user to get
    coins for an account.
    
    Co-authored-by: beerosagos <luca@bitbox.swiss>

--- frontends/web/src/routes/router.tsx
+++ frontends/web/src/routes/router.tsx
@@ -199,7 +199,8 @@ export const AppRouter = ({ devices, devicesKey, accounts, activeAccounts }: TAp
   const SwapEl = (<InjectParams>
     <Swap
       code={''}
-      accounts={activeAccounts} />
+      accounts={accounts}
+      activeAccounts={activeAccounts} />
   </InjectParams>);
 
   const MarketEl = (<InjectParams>
